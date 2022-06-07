e942fb33cf464cde377313c81da9fcdda5dbb487
Trevor Rose <topdude15@gmail.com>
2022-06-06 18:30:56 -0700

    Initial Commit

---
+++ CalendarIdea/CalendarIdeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarIdeaApp.swift
+//  CalendarIdea
+//
+//  Created by Trevor Rose on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CalendarIdeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
